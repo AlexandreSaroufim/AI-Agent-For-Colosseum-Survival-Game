8742c6cfbf09ba03526b684e4805729f8ffeb291
AlexandreSaroufim <alexandre.saroufim@mail.mcgill.ca>
2022-12-04 00:32:45 -0500

    Update student_agent.py

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -11,7 +11,9 @@ import random
 
 class Node:
     def __init__(self, state, parent):
-        self.state: list[list[list], tuple[int,int], tuple[int,int]] = state
+        #self.state: list[list[list], tuple[int, int], tuple[int, int]] = state
+        self.state = state
+        self.wins = 0
         self.wins = 0
         self.playouts = 0
         self.ucb = 0
@@ -22,13 +24,12 @@ class Node:
         self.children.append(obj)
 
     # updates ucb then returns updated values
-    def update_ucb(self,root):
+    def update_ucb(self, root):
         # can alter C to possibly improve ucb
 
         if self.playouts != 0 and root.playouts != 0:
             ucb1 = self.wins / self.playouts + math.sqrt(2) * math.sqrt(math.log(root.playouts, math.e))
             self.ucb = ucb1
-
 
         return self.ucb
 
@@ -42,9 +43,9 @@ class Monte_Carlo:
         self.max_moves = max_moves
 
         # limit to 28 seconds
-        for i in range(10):
+        for i in range(100):
             self.monte_carlo()
-            print("init: "+ str(i))
+            print("init: " + str(i))
 
     def monte_carlo(self):
 
@@ -62,7 +63,7 @@ class Monte_Carlo:
         cur_node = self.tree
         max_child_ucb: Node = None
 
-        print("\n num_children",len(cur_node.children))
+        print("\n num_children", len(cur_node.children))
         while len(cur_node.children) != 0:
             for node in cur_node.children:
                 if node.playouts == 0 or max_child_ucb is None or node.ucb > max_child_ucb.ucb:
@@ -89,16 +90,16 @@ class Monte_Carlo:
     # simulate a game using child as state
     # use random_agent using child.state
     def simulate(self, child):
-        random.choice([True,False])
+        random.choice([True, False])
 
     # Use result of the simulation to update all the search tree nodes going up to the root
     def back_propagate(self, result, child: Node):
-        
-        child.playouts+=1
 
-        #if result is true then we won, if false we didn't win
+        child.playouts += 1
+
+        # if result is true then we won, if false we didn't win
         if result:
-            child.wins+=1
+            child.wins += 1
 
         child.update_ucb(self.tree)
 
@@ -107,9 +108,25 @@ class Monte_Carlo:
 
         self.back_propagate(result, child.parent)
 
+    def actions(self,state):
 
-    def actions(self):
-        
+        #if state is anchild of root, run monte-carlo on that child as the new root
+        foundChild = False
+        for child in self.tree.children:
+            if child.state == state:
+                self.tree = child
+                foundChild = True
+                break
+
+        if not foundChild:
+            self.tree = Node(state,None)
+
+        #run monte-carlo for 1.5 seconds
+        for i in range(10):
+            self.monte_carlo()
+
+
+
         curNode: Node = self.tree
         maxChild = curNode.children[0]
 
@@ -122,23 +139,25 @@ class Monte_Carlo:
         wallsNow = self.tree.state[0][next_pos[0]][next_pos[1]]
         wallsFuture = maxChild.state[0][next_pos[0]][next_pos[1]]
 
-        #find the altered index for 2 arrays
+        # find the altered index for 2 arrays
         dir = [i for i, (x, y) in enumerate(zip(wallsNow, wallsFuture)) if x != y][0]
 
-        #discard all nodes that are maxChild's nodes because all the next moves will be children of maxChild
+        # discard all nodes that are maxChild's nodes because all the next moves will be children of maxChild
         self.tree = maxChild
 
-        return next_pos,dir
+        self.tree.parent = None
+
+        return next_pos, dir
 
     # generate a child based on the leaf node that doesn't exist in the tree
     def generate_child(self, leaf: Node):
-        #temporary
+        # temporary
         return self.random_child(leaf)
 
-
-    #fixxx
-    #generate a child that doesn't cause a suicide
-    def random_child(self, leaf:Node):
+    # fixxx
+    # generate a child that doesn't cause a suicide
+    #
+    def random_child(self, leaf: Node):
         # Moves (Up, Right, Down, Left)
 
         max_step = self.max_moves
@@ -158,14 +177,21 @@ class Monte_Carlo:
 
             # Special Case enclosed by Adversary
             k = 0
-            while my_pos[0] < 0 or my_pos[1] < 0 or chess_board[r, c, dir] or my_pos == adv_pos or all(item is True for item in chess_board[r][c]):
+            while chess_board[r, c, dir] or my_pos == adv_pos:
                 k += 1
                 if k > 300:
-                    print(k,chess_board, my_pos, adv_pos,dir,r,c)
+                    print(k, chess_board, my_pos, adv_pos, dir, r, c)
                     break
                 dir = np.random.randint(0, 4)
                 m_r, m_c = moves[dir]
                 my_pos = (r + m_r, c + m_c)
+                #x, y = my_pos
+                #while chess_board[x, y, 0] and chess_board[x, y, 1] and chess_board[x, y, 2] and chess_board[x, y, 3]:
+                #    dir = np.random.randint(0, 4)
+                #    m_r, m_c = moves[dir]
+                #    my_pos = (r + m_r, c + m_c)
+                #    x, y = my_pos
+                #    print("problem")
 
             if k > 300:
                 my_pos = ori_pos
@@ -175,21 +201,26 @@ class Monte_Carlo:
         dir = np.random.randint(0, 4)
         r, c = my_pos
 
-        while chess_board[r,c,dir]:
+        # temporary solution
+        counter = 0
+        while chess_board[r, c, dir]:
             dir = np.random.randint(0, 4)
+            counter += 1
+            if counter > 10:
+                break
+            # print("in while chess_board")
+            # print(r, " ", c, " ", dir)
+            # print(chess_board)
 
         print("deepcopy")
 
         leafCopy = Node(deepcopy(leaf.state), leaf)
-        leafCopy.state[1] = (r,c)
-        leafCopy.state[0][r][c][dir] = True
+        leafCopy.state[1] = (r, c)
+        leafCopy.state[0][r, c, dir] = True
 
         print("return")
 
         return leafCopy
-
-
-
 
 
 @register_agent("student_agent")
@@ -232,8 +263,7 @@ class StudentAgent(Agent):
         # dummy return
 
         if self.monte_carlo is None:
-            self.monte_carlo = Monte_Carlo((chess_board,my_pos,adv_pos), len(chess_board), max_moves=max_step)
+            self.monte_carlo = Monte_Carlo((chess_board, my_pos, adv_pos), len(chess_board), max_moves=max_step)
 
-        return self.monte_carlo.actions()
-        #return my_pos, self.dir_map["u"]
-
+        return self.monte_carlo.actions((chess_board,my_pos,adv_pos))
+        # return my_pos, self.dir_map["u"]
