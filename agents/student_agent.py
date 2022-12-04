3a90b6660394bee380fdb5917006f59ccc3b29bf
AlexandreSaroufim <alexandre.saroufim@mail.mcgill.ca>
2022-12-04 00:58:17 -0500

    Update student_agent.py

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -11,7 +11,7 @@ import random
 
 class Node:
     def __init__(self, state, parent):
-        #self.state: list[list[list], tuple[int, int], tuple[int, int]] = state
+        # self.state: list[list[list], tuple[int, int], tuple[int, int]] = state
         self.state = state
         self.wins = 0
         self.wins = 0
@@ -92,6 +92,69 @@ class Monte_Carlo:
     def simulate(self, child):
         random.choice([True, False])
 
+    @staticmethod
+    def check_endgame(state):
+
+        """
+                       Check if the game ends and compute the current score of the agents.
+
+                       Returns
+                       -------
+                       is_endgame : bool
+                           Whether the game ends.
+                       outcome : int
+                        Whether we win or enemy or tie
+                       """
+
+        chess_board, my_pos, enemy_pos = state
+        moves = ((-1, 0), (0, 1), (1, 0), (0, -1))
+        board_size = chess_board.shape[0]
+
+        # Union-Find
+        father = dict()
+        for r in range(board_size):
+            for c in range(board_size):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(board_size):
+            for c in range(board_size):
+                for dir, move in enumerate(
+                        moves[1:3]
+                ):  # Only check down and right
+                    if chess_board[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(board_size):
+            for c in range(board_size):
+                find((r, c))
+        p0_r = find(my_pos)
+        p1_r = find(enemy_pos)
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+        if p0_r == p1_r:
+            return False, -1
+        player_win = None
+        win_blocks = -1
+        if p0_score > p1_score:
+            return True, 0
+        elif p0_score < p1_score:
+            return True, 1
+        else:
+            player_win = -1  # Tie
+            return True, 2
+
     # Use result of the simulation to update all the search tree nodes going up to the root
     def back_propagate(self, result, child: Node):
 
@@ -108,9 +171,9 @@ class Monte_Carlo:
 
         self.back_propagate(result, child.parent)
 
-    def actions(self,state):
+    def actions(self, state):
 
-        #if state is anchild of root, run monte-carlo on that child as the new root
+        # if state is anchild of root, run monte-carlo on that child as the new root
         foundChild = False
         for child in self.tree.children:
             if child.state == state:
@@ -119,13 +182,11 @@ class Monte_Carlo:
                 break
 
         if not foundChild:
-            self.tree = Node(state,None)
+            self.tree = Node(state, None)
 
-        #run monte-carlo for 1.5 seconds
+        # run monte-carlo for 1.5 seconds
         for i in range(10):
             self.monte_carlo()
-
-
 
         curNode: Node = self.tree
         maxChild = curNode.children[0]
@@ -185,8 +246,8 @@ class Monte_Carlo:
                 dir = np.random.randint(0, 4)
                 m_r, m_c = moves[dir]
                 my_pos = (r + m_r, c + m_c)
-                #x, y = my_pos
-                #while chess_board[x, y, 0] and chess_board[x, y, 1] and chess_board[x, y, 2] and chess_board[x, y, 3]:
+                # x, y = my_pos
+                # while chess_board[x, y, 0] and chess_board[x, y, 1] and chess_board[x, y, 2] and chess_board[x, y, 3]:
                 #    dir = np.random.randint(0, 4)
                 #    m_r, m_c = moves[dir]
                 #    my_pos = (r + m_r, c + m_c)
@@ -265,5 +326,5 @@ class StudentAgent(Agent):
         if self.monte_carlo is None:
             self.monte_carlo = Monte_Carlo((chess_board, my_pos, adv_pos), len(chess_board), max_moves=max_step)
 
-        return self.monte_carlo.actions((chess_board,my_pos,adv_pos))
+        return self.monte_carlo.actions((chess_board, my_pos, adv_pos))
         # return my_pos, self.dir_map["u"]
